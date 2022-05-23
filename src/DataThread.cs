c5de5e8c4bf7200e56ab1457db0380f6706064a2
Echoviser <echoviser@gmail.com>
2022-05-23 22:19:04 +0200

    Revert test.

--- src/DataThread.cs
+++ src/DataThread.cs
@@ -104,7 +104,7 @@ namespace WSO
 
                     try
                     {
-                        using RegistryKey key = null;// localKey.OpenSubKey(keyName);
+                        using RegistryKey key = localKey.OpenSubKey(keyName);
                         if (key == null)
                         {
                             HandleServiceDataException(null, keyName);
